4e6bf8219780073bda8dfeb75705b6bc7bb3e744
Vitor Lima <vitorlimadf@outlook.com>
2021-07-26 17:16:15 -0300

    First two functions. :)

---
+++ teste1.ps1
@@ -0,0 +1,16 @@
+function get-test1 {
+    Write-Host "This is the Test1 function"
+    Start-Sleep 1 
+    Write-Host "..."
+    Clear-Host
+}
+
+function get-test2 {
+    Write-Host "This is the Test2 function"
+    Start-Sleep 2 
+    Write-Host "..."
+    Clear-Host
+}
+
+get-test1
+get-test2
